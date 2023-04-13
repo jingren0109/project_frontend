f1f34b42a0bcd5354a89006bdcbe93f687f7da0c
Jing Ren <jingren@192.168.1.190>
2023-04-13 17:04:02 +0100

    hash

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { Landing, Error, AddPrediction } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { HashRouter } from "react-router-dom";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="predict" element={<AddPrediction />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <ToastContainer position="top-center" />
-    </BrowserRouter>
+    <HashRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="predict" element={<AddPrediction />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <ToastContainer position="top-center" />
+      </BrowserRouter>
+    </HashRouter>
   );
 }
 
