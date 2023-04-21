cf91b9175327ab3101d6f460b01baec3b4fafe93
Jing Ren <jingren@192.168.1.190>
2023-04-21 10:23:21 +0100

    change url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = "https://lendwise.azurewebsites.net";
-// const baseURL = "http://127.0.0.1:5000";
+// const baseURL = "https://lendwise.azurewebsites.net";
+const baseURL = "http://127.0.0.1:5000";
 
 const customFetch = axios.create({
   baseURL: baseURL,
